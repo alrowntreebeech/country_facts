fb67c942a99d6b50df8d3ad49cbf66ba6df24956
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2023-03-21 11:10:07 +0000

    Add Navigation Bar

---
+++ src/features/navigationBar/NavigationBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./navigationBar.css";
+
+function NavigationBar() {
+    return(
+        <nav>
+            <h4>
+                Home Page
+            </h4>
+            <h4>
+                Country Search
+            </h4>
+            <h4>
+                Saved Countries
+            </h4>
+            <h4>
+                Games and Flashcards
+            </h4>
+        </nav>
+    )
+}
+
+export default NavigationBar;
