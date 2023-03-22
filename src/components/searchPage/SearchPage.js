1620ccd358c2f3f4e47167fd125e252c11d00a34
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2023-03-22 11:24:52 +0000

    Add search page and style

---
+++ src/components/searchPage/SearchPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./searchPage.css";
+
+const SearchPage = () => {
+    return(
+        <div className="searchPage">
+            <h1>Country Search</h1>
+            <div className="form">
+                <div className="input">
+                    <label htmlFor="country">Country Search:</label>
+                    <input type="text" id="country" name="country" required/>
+                </div>
+                <div className="searchButton">
+                    <input type="submit" className="button" id="countrySearchButton" value="Search"></input>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default SearchPage;
