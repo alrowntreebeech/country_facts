1257ef426f1c3834d0549afb9c6ca7a2870c486f
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2023-03-22 13:54:48 +0000

    Begin Search page Slice + delete counter

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import searchPageReducer from '../components/searchPage/searchPageSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    searchPage: searchPageReducer
   },
 });
--- src/components/searchPage/searchPageSlice.js
+++ src/components/searchPage/searchPageSlice.js
@@ -4,6 +4,16 @@ export const searchPageSlice = createSlice({
     name: 'searchPage',
     initialState: [],
     reducers: {
-        
+        displayCountries: (state, action) => {
+            state = action.payload.map(country => {
+                return ({
+                    name: country.name,
+                    flag: country.flag
+                })
+            })
+        }
     }
-})
+});
+
+export const { displayCountries } = searchPageSlice.actions;
+export default searchPageSlice.reducer;
