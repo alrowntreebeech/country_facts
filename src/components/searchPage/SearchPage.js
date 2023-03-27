7121e2a550eec2bfea257efcb78087e2f7b3d991
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2023-03-27 10:04:41 +0100

    update search page to form

--- src/components/searchPage/SearchPage.js
+++ src/components/searchPage/SearchPage.js
@@ -1,25 +1,33 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { selectCountries } from "./searchPageSlice";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectCountries, loadCountries } from "./searchPageSlice";
 import { Link } from "react-router-dom";
 import "./searchPage.css";
 
 const SearchPage = () => {
     
-    const countryResults = useSelector(selectCountries);
+    const [searchTerm, setSearchTerm] = useState(""); 
+    const dispatch = useDispatch();
+    const countryResults = useSelector(selectCountries)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+  
+    }
+
 
     return(
         <div className="searchPage">
             <h1>Country Search</h1>
-            <div className="form">
+            <form className="form">
                 <div className="input">
                     <label htmlFor="country">Country Search:</label>
-                    <input type="text" id="country" name="country" required/>
+                    <input type="text" id="country" name="country" onChange={(e) => setSearchTerm(e.currentTarget.value)} required/>
                 </div>
-                <div className="searchButton">
+                <button className="searchButton">
                     <input type="submit" className="button" id="countrySearchButton" value="Search" onSubmit={() => {}}></input>
-                </div>
-            </div>
+                </button>
+            </form>
             <div className="searchResults">
                 {countryResults.map(country => {
                     return <div className="countryResult">
--- src/components/searchPage/searchPageSlice.js
+++ src/components/searchPage/searchPageSlice.js
@@ -11,28 +11,7 @@ export const loadCountries = createAsyncThunk(
 
 export const searchPageSlice = createSlice({
     name: 'searchPage',
-    initialState: [
-        {
-            name: "Guatemala",
-            flag: "https://flagcdn.com/w320/gt.png",
-            alt: "The flag of Guatemala is composed of three equal vertical bands of light blue, white and light blue, with the national coat of arms centered in the white band."
-        },
-        {
-            name: "Germany",
-            flag: "https://flagcdn.com/w320/de.png",
-            alt: "The flag of Germany is composed of three equal horizontal bands of black, red and gold."
-        },
-        {
-            name: "Georgia",
-            flag: "https://flagcdn.com/w320/ge.png",
-            alt: "The flag of Georgia has a white field with a large centered red cross that extends to the edges and divides the field into four quarters. A small red Bolnur-Katskhuri cross is centered in each quarter."
-        },
-        {
-            name: "Gabon",
-            flag: "https://flagcdn.com/w320/ga.png",
-            alt: "The flag of Gabon is composed of three equal horizontal bands of green, yellow and blue."
-        }
-    ],
+    initialState: [],
     reducers: {
         loadCountries: (state, action) => {
             state = action.payload.map(country => {
