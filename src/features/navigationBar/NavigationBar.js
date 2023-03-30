f0c694c5f999786c86665509f16aca576e1b1e86
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2023-03-30 10:51:44 +0100

    delete games link

--- src/features/navigationBar/NavigationBar.js
+++ src/features/navigationBar/NavigationBar.js
@@ -8,7 +8,6 @@ const NavigationBar =() => {
             <NavLink to="/" className={ ({ isActive }) => isActive ? 'active-navlink' : 'inactive-navlink' }><h4>Home Page</h4></NavLink>
             <NavLink to="/search" className={ ({ isActive }) => isActive ? 'active-navlink' : 'inactive-navlink' } ><h4>Country Search</h4></NavLink>
             <NavLink to="working" className={ ({ isActive }) => isActive ? 'active-navlink' : 'inactive-navlink' }><h4>Saved Countries</h4></NavLink>
-            <NavLink to="working" className={ ({ isActive }) => isActive ? 'active-navlink' : 'inactive-navlink' }><h4>Games and Flashcards</h4></NavLink>
         </nav>
     )
 }
